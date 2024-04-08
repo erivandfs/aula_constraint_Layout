cb4c30ef6e8cea21b18978877cdc164dfb33d5f3
erivan <erivan.santos@unemat.br>
2024-04-08 07:31:00 -0400

    inteface

---
+++ app/src/main/java/br/com/app1/aula_constraint_layout/MainActivity.java
@@ -0,0 +1,22 @@
+package br.com.app1.aula_constraint_layout;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        setContentView(R.layout.activity_main);
+
+        });
+    }
+}
